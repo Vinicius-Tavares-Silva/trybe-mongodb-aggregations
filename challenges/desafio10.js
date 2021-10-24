6d30c70280e977f7e068601e03ab3a2fe2a62c11
Vinicius Tavares <vini_tavares10@hotmail.com>
2021-10-24 18:34:41 -0300

    Requisito 10

---
+++ challenges/desafio10.js
@@ -0,0 +1,30 @@
+db.trips.aggregate([
+  {
+    $group: {
+      _id: "$usertype",
+      duracaoMedia: {
+        $avg: {
+          $dateDiff: {
+            startDate: "$startTime",
+            endDate: "$stopTime",
+            unit: "hour",
+          },
+        },
+      },
+    },
+  },
+  {
+    $sort: {
+      duracaoMedia: 1,
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      tipo: "$_id",
+      duracaoMedia: {
+        $round: ["$duracaoMedia", 2],
+      },
+    },
+  },
+]);
